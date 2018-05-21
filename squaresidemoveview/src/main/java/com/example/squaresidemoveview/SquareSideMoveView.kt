64fcc91ad3d61ef3856a88de46841309f6cce5f2
Anwesh43 <anweshthecool0@gmail.com>
2018-05-22 00:57:55 +0530

    adding SquareSideMoveView class

--- squaresidemoveview/src/main/java/com/example/squaresidemoveview/SquareSideMoveView.kt
+++ squaresidemoveview/src/main/java/com/example/squaresidemoveview/SquareSideMoveView.kt
@@ -3,3 +3,26 @@ package com.example.squaresidemoveview
 /**
  * Created by anweshmishra on 22/05/18.
  */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.*
+
+class SquareSideMoveView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
