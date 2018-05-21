e8cbe15112bfb8eef83041b57a05b4dd4ae54a55
Anwesh43 <anweshthecool0@gmail.com>
2018-05-22 01:42:49 +0530

    adding view in activity

--- app/src/main/java/com/example/anweshmishra/kotlinsquaresidemoveview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinsquaresidemoveview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.anweshmishra.kotlinsquaresidemoveview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.squaresidemoveview.SquareSideMoveView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareSideMoveView.create(this)
     }
 }
